17303f47894d4a5248e5a389c25dc97d27ebb9c6
edmarsht <edtoulet@gmail.com>
2021-11-15 19:21:07 +0100

    initialize commit

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import faker from "faker";
+import "./Header.css";
+import Story from "./Story.js";
+
+function Header() {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(50)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+    setSuggestions(suggestions);
+  }, []);
+
+  return (
+    <div className="header">
+      {/* <h1>Hey ! That's my Faker person here </h1> */}
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Header;
